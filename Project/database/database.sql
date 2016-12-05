8fa13ab87af44422edd504ce1b370062e7573bb0
Ines Gomes <gomes.inesisabel@gmail.com>
2016-12-05 15:05:15 +0000

    tentativa falhada de testar as cenas inserindo valores na bd.

--- Project/database/database.sql
+++ Project/database/database.sql
@@ -1,15 +1,5 @@
 PRAGMA FOREIGN_KEYS = ON;
 
-CREATE TABLE Review (
-	reviewId INTEGER PRIMARY KEY AUTOINCREMENT,
-	username NVARCHAR2(100),
-	restaurantId INTEGER,
-	rating INTEGER NOT NULL CHECK (rating > 0 AND rating < 10),
-	text NVARCHAR(400),
-	FOREIGN KEY(restaurantId) REFERENCES Restaurant(restaurantId) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY(username) REFERENCES User(username) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
 CREATE TABLE User (
 	userId INTEGER PRIMARY KEY AUTOINCREMENT,
 	name NVARCHAR2(100) NOT NULL,
@@ -37,6 +27,16 @@ CREATE TABLE Restaurant (
 	rating_total REAL CHECK (rating_total > 0),
 	owner NVARCHAR2(100),
 	FOREIGN KEY(owner) REFERENCES User(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Review (
+	reviewId INTEGER PRIMARY KEY AUTOINCREMENT,
+	username NVARCHAR2(100),
+	restaurantId INTEGER,
+	rating INTEGER NOT NULL CHECK (rating > 0 AND rating < 10),
+	text NVARCHAR(400),
+	FOREIGN KEY(restaurantId) REFERENCES Restaurant(restaurantId) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(username) REFERENCES User(username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE FoodType (
@@ -74,3 +74,14 @@ CREATE TABLE ReviewPhoto (
 	FOREIGN KEY(username) REFERENCES User(username) ON DELETE CASCADE ON UPDATE CASCADE
 	
 );
+
+INSERT INTO Photo VALUES (1,'../css/Images/default.png');
+
+INSERT INTO User VALUES (NULL, 'ines','ola@a.com','01-01-2016','1234-123','ines','ines',1);
+INSERT INTO User VALUES (NULL, 'andreia','ola@a.com','01-01-2016','1234-123','andreia','andreia',1);
+
+INSERT INTO Restaurant VALUES (1,'montaditos','restaurante espanhol','Porto','123456789','5','24h',NULL,NULL,1,7,7,'andreia');
+
+INSERT INTO Review VALUES (1,'ines',1,8,'rapido e bom');
+
+INSERT INTO ReviewPhoto VALUES (1,1,'ines');
