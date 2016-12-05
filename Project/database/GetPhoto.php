5bee5406424776a0327af92d70bf559c7f64252b
xCutePsycho <andreiacarodrigues@gmail.com>
2016-12-05 19:51:10 +0000

    faltavam cenas

---
+++ Project/database/GetPhoto.php
@@ -0,0 +1,15 @@
+﻿<?php
+include_once('Connection.php');
+
+	global $db;
+	$id = $_GET["id"];
+	$stmt = $db->prepare("SELECT filename FROM Photo WHERE photoId = ?");
+	$stmt->execute(array($id));
+	$info = $stmt->fetch();
+	
+	
+	if(!empty($info))
+		echo json_encode($info['filename']);
+	else
+		echo 'INVALID';
+?>
