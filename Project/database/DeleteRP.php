9ce49c20f5bacece5ebbebc021076d6cd6a839e7
Ines Gomes <gomes.inesisabel@gmail.com>
2016-12-11 17:15:59 +0000

    Correção.

--- Project/database/DeleteRP.php
+++ Project/database/DeleteRP.php
@@ -6,7 +6,7 @@ include_once('my_database/Photo.php');
 $val = $_POST['val'];
 $res = $_POST['restaurant'];
 
-$deleteId = getPhotoByName($val);
+$deleteId = getPhotoByFilename($val);
 $deleteId = $deleteId['photoId'];
 
 deletePhoto($deleteId);
